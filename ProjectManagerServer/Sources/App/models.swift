438b1259aef9c480ea8d9290eba6dfb229bf853c
babywalnut <anfuza@naver.com>
2021-08-04 15:20:32 +0900

    [#3]feat: state 속성 enum 변경후 마이그레이션 models.swift: 	- enum State추가 	- tasks model에서 index_path 삭제 	- tasks model의 due_date timeStamp -> Field 어노테이션 변경 	- CreateTask Migration 타입 수정

--- ProjectManagerServer/Sources/App/models.swift
+++ ProjectManagerServer/Sources/App/models.swift
@@ -8,6 +8,12 @@
 import Vapor
 import Fluent
 
+//class SomeClass: Validatable
+
+enum State: String, Codable {
+    case todo, doing, done
+}
+
 final class Task: Model, Content {
 
     static var schema: String = "tasks"
@@ -21,40 +27,44 @@ final class Task: Model, Content {
     @Field(key: "body")
     var body: String
     
-    @Timestamp(key: "due_date", on: .create)
-    var due_date: Date?
+    @Field(key: "due_date")
+    var due_date: Date
     
-    @Field(key: "state")
-    var state: String
-    
-    @Field(key: "index_path")
-    var index_path: Int
-    
+    @Enum(key: "state")
+    var state: State
+
     init() { }
     
-    init(id: UUID? = nil, title: String, body: String, due_date: Date? = nil, state: String, index_path: Int) {
+    init(id: UUID? = nil, title: String, body: String, due_date: Date, state: State) {
         self.id = id
         self.title = title
         self.body = body
         self.due_date = due_date
         self.state = state
-        self.index_path = index_path
     }
 }
 
 struct CreateTask: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("tasks")
-            .id()
-            .field("title", .string, .required)
-            .field("body", .string)
-            .field("due_date", .datetime, .required)
-            .field("state", .string, .required)
-            .field("index_path", .int, .required)
+        return database.enum("state")
+            .case("todo")
+            .case("doing")
+            .case("done")
             .create()
+            .flatMap { state in
+                return database.schema("tasks")
+                    .id()
+                    .field("title", .string, .required)
+                    .field("body", .string)
+                    .field("due_date", .date, .required)
+                    .field("state", .string, .required)
+                    .create()
+            }
     }
     
     func revert(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("tasks").delete()
+        return database.schema("tasks").delete().flatMap {
+            return database.enum("state").delete()
+        }
     }
 }
